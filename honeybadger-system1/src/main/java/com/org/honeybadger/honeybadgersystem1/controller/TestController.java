96f0616ae27e5ecc9abbe6f98dc37473f279019b
yangjun1qaz <1094557609@qq.com>
2019-08-14 17:09:50 +0800

    增加多环境配置
    Signed-off-by: yangjun1qaz <1094557609@qq.com>

--- honeybadger-system1/src/main/java/com/org/honeybadger/honeybadgersystem1/controller/TestController.java
+++ honeybadger-system1/src/main/java/com/org/honeybadger/honeybadgersystem1/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.org.honeybadger.honeybadgersystem1.controller;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RestController
 public class TestController {
-
 
     @Value("${testyangqi.name}")
     private String userName;
